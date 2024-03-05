8d3e615279c21b64876eb417ffb72689c499639b
jamesli2021 <95068031+jamesli2021@users.noreply.github.com>
2024-03-05 09:24:19 +0800

    Add files via upload

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import cloudflare from "@astrojs/cloudflare";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: "directory",
-  adapter: cloudflare()
+  output: "server",
+  adapter: cloudflare({ "directory" })
 });
